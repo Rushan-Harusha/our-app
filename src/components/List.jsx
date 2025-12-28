a39b7f4e6aab5bbbe95928e5013b295b5a3c0391
Rushan <rushanharusha123@gmail.com>
2025-12-28 13:22:32 +0530

    feat: add reusable List component

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+function List({ items, renderItem, emptyMessage = "No items found" }) {
+  if (!items.length) return <p>{emptyMessage}</p>;
+
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default List;
